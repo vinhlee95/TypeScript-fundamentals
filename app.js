35df500445ba7ded09928e9ba683a323e95a0b9b
Vinhlee95 <lethanhvinh95@gmail.com>
2018-10-11 10:31:21 +0300

    practice some basic types

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hi there');
+var myName;
+myName = 23;
+myName = 'hello world';
+var hobby = ['Hi', 'there'];
+function logMessage() {
+    console.log('Hi there');
+}
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+var myMultiple;
+myMultiple = multiply;
+console.log(myMultiple(1, 2));
--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-console.log('hi there')
+let myName;
+myName = 23
+myName = 'hello world'
+
+const hobby: string[] = ['Hi', 'there']
+
+function logMessage() :void {
+  console.log('Hi there')
+}
+
+// function type
+function multiply(val1:number, val2:number) :number {
+  return val1 * val2
+}
+
+let myMultiple :(val1:number, val2:number) => number;
+myMultiple = multiply;
+console.log(myMultiple(1,2))
+
+// union type
+let myVar: string | number = 'Hi there'
+myVar = 23
+myVar = false
+
+// strict null check
+let foo = 12
+foo = null
